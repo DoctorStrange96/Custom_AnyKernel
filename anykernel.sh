9679425c0b0517a89e04aca0b82a1f1658d280c2
Kamin4ri <kaminari.k2@gmail.com>
2016-01-08 23:21:03 -0300

    First commit
    Signed-off-by: Kamin4ri <kaminari.k2@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,179 @@
+# AnyKernel 2.0 Ramdisk Mod Script 
+# osm0sis @ xda-developers
+# Modified for the Moto G by KingKaminari @ xda-developers
+
+# Device variables
+device=`getprop ro.product.device`;
+prodname=`grep ^ro.product.name /system/build.prop | cut -d= -f2`;
+filesystem=`mount | grep /data | cut -c37-40`;
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+
+## end setup
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+cd $ramdisk;
+
+OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
+ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+    dd if=$block of=/tmp/anykernel/boot.img;
+    $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+    if [ $? != 0 ]; then
+        ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
+        echo "Dumping/unpacking image failed. Aborting..." > /tmp/anykernel/error;
+        echo 1 > /tmp/anykernel/exitcode; exit;
+    fi;
+    gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+    cd $split_img;
+    cmdline=`cat *-cmdline`;
+    board=`cat *-board`;
+    base=`cat *-base`;
+    pagesize=`cat *-pagesize`;
+    kerneloff=`cat *-kerneloff`;
+    ramdiskoff=`cat *-ramdiskoff`;
+    tagsoff=`cat *-tagsoff`;
+    if [ -f *-second ]; then
+        second=`ls *-second`;
+        second="--second $split_img/$second";
+        secondoff=`cat *-secondoff`;
+        secondoff="--second_offset $secondoff";
+    fi;
+    # Falcon requires a zImage with the device tree(s) appended, so let's use it
+    if [ -f /tmp/anykernel/zImage-dtb ]; then
+        kernel=/tmp/anykernel/zImage-dtb; 
+    else
+        kernel=`ls *-zImage-dtb`;
+        kernel=$split_img/$kernel;
+	fi;
+	cd $ramdisk;
+    find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+    $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff --output /tmp/anykernel/boot-new.img;
+    if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+        ui_print " "; ui_print "Repacking image failed. Aborting...";
+        echo "Repacking image failed. Aborting..." > /tmp/anykernel/error;        
+        echo 1 > /tmp/anykernel/exitcode; exit;
+    fi;
+    dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# insert_line <file> <if search string> <before/after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5};" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+	if [ ! -z "$(grep "$2" $1)" ]; then
+    	line=`grep -n "$2" $1 | cut -d: -f1`;
+		sed -i "${line}d" $1;
+	fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+	if [ -z "$(grep "$2" $1)" ]; then
+    	echo "$(cat $patch/$3 $1)" > $1;
+	fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+	if [ -z "$(grep "$2" $1)" ]; then
+		echo -ne "\n" >> $1;
+		cat $patch/$3 >> $1;
+		echo -ne "\n" >> $1;
+	fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+	cp -fp $patch/$3 $1;
+	chmod $2 $1;
+}
+
+## end methods
+
+
+## AnyKernel permissions
+# set permissions for included files
+chmod -R 755 $ramdisk;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# kernel scripts / init.d support
+# Use the proper init.rc and fstab.qcom files for each rom (stock/gpe)
+if [ $prodname == "falcon_gpe" -o $device == "falcon_gpe" ]; then
+	# Patch init.rc to add init.d support & execute tweak script
+	replace_file init.rc 750 gpe/init.rc;
+	echo "Init.rc file used: GPE" >> /tmp/recovery.log;	
+	# Use modified fstab if /data is formatted to f2fs
+	if [ $filesystem = "f2fs" ]; then
+		replace_file fstab.qcom 640 gpe/fstab_f2fs.qcom;
+	# Revert to the original fstab if /data is/has been formatted to ext4	
+	elif [ $filesystem = "ext4" ]; then
+		replace_file fstab.qcom 640 gpe/fstab_ext4.qcom;
+	fi;
+else
+	# Patch init.rc to add init.d support & execute tweak script
+	replace_file init.rc 750 stock/init.rc;
+	echo "Init.rc file used: Regular" >> /tmp/recovery.log;
+	# Patch init.mmi.rc to avoid I/O scheduler override
+	replace_file init.mmi.rc 750 stock/init.mmi.rc;
+	# Use modified fstab if /data is formatted to ext4	
+	if [ $filesystem = "ext4" ]; then
+		replace_file fstab.qcom 640 stock/fstab_ext4.qcom;
+	# Revert to the original fstab if /data is/has been formatted to f2fs
+	elif [ $filesystem = "f2fs" ]; then
+		replace_file fstab.qcom 640 stock/fstab_f2fs.qcom;
+	fi;
+fi;
+
+# end ramdisk changes
+write_boot;
+
+## end install
+
---
+++ ramdisk/init.kaminari.sh
@@ -0,0 +1,77 @@
+#!/system/bin/sh
+
+# Function to output stuff to dmesg
+function kmesg() {
+	$bb echo $1 | $bb tee /dev/kmsg;
+}
+
+# Custom busybox path
+bb=/sbin/bb/busybox;
+
+# Enable Fsync
+$bb echo "Y" > /sys/module/sync/parameters/fsync_enabled;
+
+# Disable gentle fair sleepers
+$bb echo "NO_GENTLE_FAIR_SLEEPERS" > /sys/kernel/debug/sched_features;
+$bb echo "0" > /sys/kernel/sched/gentle_fair_sleepers;
+
+# Enable Headset High Performance Mode
+$bb echo "1" > /sys/devices/virtual/misc/soundcontrol/highperf_enabled;
+
+# Enable arch power
+$bb echo "ARCH_POWER" > /sys/kernel/debug/sched_features;
+$bb echo "1" > /sys/kernel/sched/arch_power;
+
+# GPU Settings
+$bb echo "msm-adreno-tz" > /sys/devices/fdb00000.qcom,kgsl-3d0/devfreq/fdb00000.qcom,kgsl-3d0/governor;
+
+# Enable Adreno Idler
+$bb echo "Y" > /sys/module/adreno_idler/parameters/adreno_idler_active;
+
+# Set TCP congestion control algo to westwood
+$bb echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control;
+
+# Disable debugging on some modules
+$bb echo "0" > /sys/module/kernel/parameters/initcall_debug;
+$bb echo "0" > /sys/module/alarm/parameters/debug_mask;
+$bb echo "0" > /sys/module/alarm_dev/parameters/debug_mask;
+$bb echo "0" > /sys/module/binder/parameters/debug_mask;
+$bb echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask;
+
+# Power Mode
+$bb echo "1" > /sys/module/msm_pm/modes/cpu0/retention/idle_enabled;
+$bb echo "1" > /sys/module/msm_pm/modes/cpu1/retention/idle_enabled;
+$bb echo "1" > /sys/module/msm_pm/modes/cpu2/retention/idle_enabled;
+$bb echo "1" > /sys/module/msm_pm/modes/cpu3/retention/idle_enabled;
+
+# Set i/o scheduler & read-ahead memory size
+$bb echo "zen" > /sys/block/mmcblk0/queue/scheduler;
+$bb echo "128" > /sys/block/mmcblk0/bdi/read_ahead_kb;
+
+# Enable Multicore Power Saving
+$bb echo "1" > /sys/devices/system/cpu/sched_mc_power_savings;
+
+# Wait for systemui and increase its priority
+while sleep 1; do
+  if [ `$bb pidof com.android.systemui` ]; then
+    systemui=`$bb pidof com.android.systemui`;
+    $bb renice -18 $systemui;
+    $bb echo "-17" > /proc/$systemui/oom_adj;
+    $bb chmod 100 /proc/$systemui/oom_adj;
+    exit;
+  fi;
+done &
+
+# LMK whitelist for common launchers and increase launcher priority
+list="com.android.launcher com.google.android.googlequicksearchbox org.adw.launcher org.adwfreak.launcher net.alamoapps.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.gtp.nextlauncher com.miui.mihome2 com.mobint.hololauncher com.mobint.hololauncher.hd com.qihoo360.launcher com.teslacoilsw.launcher com.tsf.shell org.zeam";
+while sleep 60; do
+  for class in $list; do
+    if [ `$bb pgrep $class | $bb head -n 1` ]; then
+      launcher=`$bb pgrep $class`;
+      $bb echo "-17" > /proc/$launcher/oom_adj;
+      $bb chmod 100 /proc/$launcher/oom_adj;
+      $bb renice -18 $launcher;
+    fi;
+  done;
+  exit;
+done &
